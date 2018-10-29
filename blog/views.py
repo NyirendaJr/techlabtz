c63347b9d4c6a6231fcbc780c7f46a3b777a0eb9
NyirendaJr <javaxynyirenda@gmail.com>
2018-10-29 09:04:15 +0300

    modification of templates

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,20 @@ from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    posts = Post.objects.exclude(published_date__isnull=True).order_by('-published_date')
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'blog/post_list.html', context)
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/blog-post.html', {'post': post})
 
 
 @login_required
@@ -24,7 +28,7 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_draft_list')
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
@@ -39,7 +43,7 @@ def post_edit(request, pk):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_draft_list')
     else:
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
@@ -47,7 +51,8 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    user = request.user
+    posts = Post.objects.all().filter(author=user).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
@@ -55,14 +60,14 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail', pk=pk)
+    return redirect('blog:post_draft_list')
 
 
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
-    return redirect('post_list')
+    return redirect('blog:post_draft_list')
 
 
 def add_comment_to_post(request, pk):
@@ -70,10 +75,14 @@ def add_comment_to_post(request, pk):
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
+            comment = Comment()
             comment = form.save(commit=False)
             comment.post = post
+            comment.author = form.cleaned_data['author']
+            comment.text = form.cleaned_data['text']
+            comment.created_date = timezone.now()
             comment.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
@@ -84,11 +93,13 @@ def add_comment_to_post(request, pk):
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
+    return redirect('blog:post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail', pk=comment.post.pk)
-       
+    return redirect('blog:post_detail', pk=comment.post.pk)
+
+def author_details(request, pk):
+    return HttpResponse(pk)
